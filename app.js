121bdb3878b7ad3ce86309968f92b9946733fe67
samuel-tobi <samueltobi52@gmail.com>
2022-09-25 00:04:21 +0100

    styled player one container

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 const getStarted = document.getElementById("getStarted");
 // const playerOne = document.getElementById("playerOne");
 const playerOneInput = document.getElementById("playerOne-input");
-const submitPlayerOne = document.querySelector("#playerOne-form");
+// const submitPlayerOne = document.querySelector("#playerOne-form");
 // const playerTwo = document.getElementById("playerTwo");
-
-// const git = new Git();
-// const userUI = new UserUI();
-
-//users object
-const users = {
-  playerOne: "",
-  playerTwo: "",
-};
 
 function showPlayerOne() {
   //Show player one
@@ -22,17 +13,52 @@ function showPlayerOne() {
 }
 getStarted.addEventListener("click", showPlayerOne);
 
-// function submitPlayerOne() {
-//   // Submit Player One
-//   const submitPlayerOne = document.querySelector("#playerOne-form");
-//   submitPlayerOne.addEventListener("submit", (e) => {
-//     e.preventDefault();
+// function fetchPlayerOne() {}
 
-//     username.playerOne = playerOneInput.value.trim();
+// Submit Player One
+const submitPlayerOne = document.querySelector("#playerOneForm");
+submitPlayerOne.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Fetch Player One
+  const playerone = document.getElementById("playerOne-input").value;
+  const playeronename = playerone.split(" ").join("");
+  fetch("https://api.github.com/users/" + playeronename)
+    .then((result) => result.json())
+    .then((data) => {
+      console.log(data);
+      const playerOneProfile = document.getElementById("usersUI");
+      playerOneProfile.innerHTML = `
+      <div class='playerOne-container'>
+      <img src=${data.avatar_url}/>
+      <ul class="lists">
+          <li class="lists-item">Name: ${data.name}</li>
+          <li class="lists-item">Username: ${data.login}</li>
+          <li class="lists-item">Followers: ${data.followers}</li>
+          <li class="lists-item">Following: ${data.following}</li>
+          <li class="lists-item">Public Repo: ${data.public_repos}</li>
+      </ul>
+      </div>`;
+    });
 
-//     // Fetch Player One
-//     git.getUse(username.playerOne).then((input) => {
-//       users.playerOne = input.user;
-//     });
-//   });
-// }
+  function hidePlayerOne() {
+    const playerOne = document.getElementById("playerOne");
+    playerOne.style.display = "none";
+  }
+  setTimeout(() => {
+    hidePlayerOne();
+  }, 1000);
+
+  const playerTwo = document.querySelector("#playerTwo");
+  playerTwo.style.display = "block";
+});
+
+// https://api.github.com/users/username
+
+function fetchPlayerTwo() {
+  const submitPlayerTwo = document.querySelector("#playerTwoForm");
+  submitPlayerTwo.addEventListener("click", (e) => {
+    e.preventDefault();
+    //
+    console.log("working");
+  });
+}
--- userUI.js
+++ userUI.js
@@ -2,7 +2,7 @@ class userUI {
   constructor() {
     this.user = document.querySelector("#usersUI");
   }
-  displayUser(user1, user2) {
+  displayUser() {
     this.user.innerHTML = `
       <h1>Confirm Players</h1>
         <div>users-container
@@ -10,11 +10,11 @@ class userUI {
             <p class='subTitle'>Player 1</p>
             <div class="user-box">
               <ul class="lists">
-                <li class='lists-item><img/></li>
-                <li class='lists-item>Username:</li>
-                <li class='lists-item>Followers:</li>
-                <li class='lists-item>Following:</li>
-                <li class='lists-item>Public Repos:</li>
+              <li class="lists-item">Name: </li>
+              <li class="lists-item">Username: </li>
+              <li class="lists-item">Followers: </li>
+              <li class="lists-item">Following: </li>
+              <li class="lists-item">Public Repos:</li>
               </ul>
             </div>
           </div>
@@ -22,21 +22,59 @@ class userUI {
             <p class='subTitle'>Player 2</p>
             <div class="user-box">
               <ul class="lists">
-                <li class='lists-item><img src=""/></li>
-                <li class='lists-item>Username:</li>
-                <li class='lists-item>Followers:</li>
-                <li class='lists-item>Following:</li>
-                <li class='lists-item>Public Repos:</li>
+              <li class="lists-item">Name: </li>
+              <li class="lists-item">Username: </li>
+              <li class="lists-item">Followers: </li>
+              <li class="lists-item">Following: </li>
+              <li class="lists-item">Public Repos:</li>
               </ul>
             </div>
           </div>
         </div>
         <div class='battle-button'>
-          <button >Initiate Battle</button>
+          <button>Initiate Battle</button>
         </div>
         <div>
-          <button >Reselect Players</button>
+          <button> Reselect Players</button>
         </div>
       `;
+  }
+  displayUsersResult() {
+    this.profile.innerHTML = `
+    <h1 class="lead">Result</h1>
+        <div class="users-container">
+          <div id="player-one" class="user">
+            <p class="subTitle">Winner</p>
+            <div class="user-box">
+              <ul class="lists">
+                <li class="lists-item"> <h3> Score: </h3> </li>
+                <li class="lists-item"> <img src=/> </li>
+                <li class="lists-item">Name: </li>
+                <li class="lists-item">Username: </li>
+                <li class="lists-item">Followers: </li>
+                <li class="lists-item">Following: </li>
+                <li class="lists-item">Public Repos:</li>
+              </ul>
+            </div>
+          </div>
+          <div id="player-two" class="user">
+            <p class="subTitle">Loser</p>
+            <div class="user-box">
+              <ul class="lists">
+              <li class="lists-item> <h3> Score: </h3></li>
+                <li class="lists-item"> <img /> </li>
+                <li class="lists-item">Name: </li>
+                <li class="lists-item">Username: </li>
+                <li class="lists-item">Followers: </li>
+                <li class="lists-item">Following: </li>
+                <li class="lists-item">Public Repos:</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="resetGame">
+          <button> Start Over </button>
+        </div>
+    `;
   }
 }
